42d3f02da6ae46e2304943c6754f60050eaa6e5c
Julian Hernandez <julian1@cimarron.me>
2019-04-17 15:52:18 -0700

    fix CI tests

--- tests/utils/test_aws.py
+++ tests/utils/test_aws.py
@@ -1,7 +1,8 @@
 import pytest
+import os
 
 
-def test_ses_send_email(mocker):
+def test_ses_send_email(mocker, monkeypatch):
     from utils.aws import ses, ses_send_email, _ses_send_email_kwargs
     mocker.patch.object(ses, 'send_email', return_value='this is a test')
     # args
@@ -9,6 +10,9 @@ def test_ses_send_email(mocker):
     email_to = ['test@test.com']
     subject = 'test subject line'
     message = 'test message body'
+
+    monkeypatch.setattr(os, 'environ', dict(CONTACT_EMAIL_FROM='test@email.com'))
+    print('\ntesting', os.environ['CONTACT_EMAIL_FROM'], end='\n')
 
     ses_send_email(reply_to, email_to, subject, message)
 
