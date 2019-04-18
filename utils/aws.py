517eda9d8ea02694211edfca47d5f94036d2cb8f
Julian Hernandez <julian1@cimarron.me>
2019-04-17 19:13:42 -0700

    refactor utils.aws

--- utils/aws.py
+++ utils/aws.py
@@ -3,17 +3,7 @@ import boto3
 from typing import List
 
 
-ses = boto3.client('ses')
-
-
-# surround in a try/catch
-def ses_send_email(reply_to: List, email_to: List, subject: str, message: str) -> None:
-    ses.send_email(
-        **_ses_send_email_kwargs(reply_to, email_to, subject, message)
-    )
-
-
-def _ses_send_email_kwargs(reply_to, email_to, subject, message):
+def ses_send_email_kwargs(reply_to, email_to, subject, message):
     return dict(
         Source=os.environ['CONTACT_EMAIL_FROM'],
         ReplyToAddresses=reply_to,
