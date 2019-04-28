732e344eb27bbd7fa5e8987f6997780ba0c4e2c1
Julian Hernandez <julian1@cimarron.me>
2019-04-27 17:01:01 -0700

    Sentry added to application

--- tests/app/test_application.py
+++ tests/app/test_application.py
@@ -1,18 +1,22 @@
 import re
 import os
+import sys
 import pytest
+import importlib
 
 
 class TestCorsPolicy:
-    host_dev = 'http://localhost'
-    host_prod = 'https://www.cimarron.me'
+
+    HOST_DEV = 'http://localhost'
+    HOST_PROD = 'https://www.cimarron.me'
+
 
     def test_cors_dev_re(self):
         from app.application import cors_origin_dev_re
-        assert bool(re.search(cors_origin_dev_re, self.host_dev))
-        assert bool(re.search(cors_origin_dev_re, f'{self.host_dev}/'))
-        assert bool(re.search(cors_origin_dev_re, f'{self.host_dev}:3015'))
-        assert bool(re.search(cors_origin_dev_re, f'{self.host_dev}:3015/this/that'))
+        assert bool(re.search(cors_origin_dev_re, self.HOST_DEV))
+        assert bool(re.search(cors_origin_dev_re, f'{self.HOST_DEV}/'))
+        assert bool(re.search(cors_origin_dev_re, f'{self.HOST_DEV}:3015'))
+        assert bool(re.search(cors_origin_dev_re, f'{self.HOST_DEV}:3015/this/that'))
 
         assert bool(re.search(cors_origin_dev_re, 'https://localhost')) == False
         assert bool(re.search(cors_origin_dev_re, 'https://any')) == False
@@ -20,33 +24,30 @@ class TestCorsPolicy:
 
     def test_cors_prod_re(self):
         from app.application import cors_origin_prod_re
-        assert bool(re.search(cors_origin_prod_re, self.host_prod))
-        assert bool(re.search(cors_origin_prod_re, f'{self.host_prod}/'))
-        assert bool(re.search(cors_origin_prod_re, f'{self.host_prod}/this/that'))
-        assert bool(re.search(cors_origin_prod_re, f'{self.host_prod}/this/that/'))
+        assert bool(re.search(cors_origin_prod_re, self.HOST_PROD))
+        assert bool(re.search(cors_origin_prod_re, f'{self.HOST_PROD}/'))
+        assert bool(re.search(cors_origin_prod_re, f'{self.HOST_PROD}/this/that'))
+        assert bool(re.search(cors_origin_prod_re, f'{self.HOST_PROD}/this/that/'))
 
         assert bool(re.search(cors_origin_prod_re, 'http://www.cimarron.me')) == False
         assert bool(re.search(cors_origin_prod_re, 'https://cimarron.me')) == False
         assert bool(re.search(cors_origin_prod_re, 'https://any.com')) == False
 
 
-    def test_dev_request(self, client):
-        if client.application.config.get('ENV') == 'production':
-            pytest.skip()
-
-        res = client.get('/', headers={ 'Origin': self.host_dev })
+    def test_dev_request(self, get_client, monkeypatch):
+        client = get_client('development')
+        res = client.get('/', headers={ 'Origin': self.HOST_DEV })
         cors_header = 'Access-Control-Allow-Origin'
         assert res.headers.has_key(cors_header)
-        assert res.headers[cors_header] == self.host_dev
+        assert res.headers[cors_header] == self.HOST_DEV
 
-    def test_prod_request(self, client):
-        if client.application.config.get('ENV') != 'production':
-            pytest.skip()
 
-        res = client.get('/', headers={ 'Origin': self.host_prod })
+    def test_prod_request(self, get_client, monkeypatch):
+        client = get_client('production')
+        res = client.get('/', headers={ 'Origin': self.HOST_PROD })
         cors_header = 'Access-Control-Allow-Origin'
         assert res.headers.has_key(cors_header)
-        assert res.headers[cors_header] == self.host_prod
+        assert res.headers[cors_header] == self.HOST_PROD
 
 
 def test_get_index(client):
